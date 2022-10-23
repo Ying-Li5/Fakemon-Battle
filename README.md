a44725bba59c754b493ff8de4ee938441c3dba75
Ying <ying__li@hotmail.com>
2022-10-23 19:08:37 -0400

    add-scoreboard

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Pokemon-Battle
+# Fakèmon-Battle
+
+INSPIRATION 
+    Growing up, I spent alot on time playing Pokèmon on my DS (particularly HeartGold/SoulSilver)
+    Yeah... so here's Fakèmon lol.
+
+HOW TO PLAY: 
+
+This is a PVP game:
+    -Player One gets to be Pikachu
+    -Player Two gets to be Charmander
+
+Relatively easy, you just wanna beat your opponent. 
+Attack damage are randomly generated with a the damage range of 1-10
+
+If player 1 wins then player 2 loses and vice versa.
+
+
+UNFINISHED FUNCTIONALITY 
+
+
+BUGS
--- script.js
+++ script.js
@@ -1,21 +1,56 @@
+//HEALTH BAR
 let charmander = {
     name: "charmander",
     health: 50,
-    damage: 10
+    damage () {
+        return Math.round((Math.random() * 10))
+    }
 }
 
 let pikachu = {
     name: "pikachu",
     health: 50,
-    damage: 10
-}
-
-
-function attackCommand(attacker, defender){
-    if (attacker === "pikachu"){
-        console.log("in the pikachu attack")
-        const newHealth = charmander.health -= pikachu.damage
-        const healthField = document.getElementById("charmander-health")
-        healthField.innerText = newHealth
+    damage () {
+        return Math.round((Math.random() * 10))
     }
 }
+
+//SCOREBOARD
+let pikachuScore = 0
+let charmanderScore = 0
+
+function scoreCounter() {
+
+}
+
+//PIKACHU ATTACKING 
+function pikachuAttack(attacker, defender){
+    if (attacker === "pikachu"){
+        console.log("in the pikachu attack")
+        let newHealth = Math.round(charmander.health -= pikachu.damage())
+        if (newHealth <= 0) {
+            newHealth = 0
+        }
+        charmander.health = newHealth
+        const healthBar = document.getElementById("charmander-health")
+        healthBar.innerText = newHealth
+    } 
+    if (charmander.health <= 0){
+        console.log("PIKACHU WIN!")
+    }
+}
+
+//CHARMANDER ATTACKING
+function charmanderAttack (attacker, defender) {
+    if (attacker === "charmander"){
+        console.log("in the charmander attack")
+        const newHealth = Math.round(pikachu.health -= charmander.damage())
+        pikachu.health = newHealth
+        const healthBar = document.getElementById("pikachu-health")
+        healthBar.innerText = newHealth
+    }
+    if (pikachu.health <= 0){
+        console.log("CHARMANDER WIN!")
+    }
+}
+
