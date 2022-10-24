2bcc2e2cbfe8c2dc71e7f4d9e33ea65c75937eed
Ying <ying__li@hotmail.com>
2022-10-23 23:14:26 -0400

    tracking-turns

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ let pikachu = {
     }
 }
 
+//ANNOUNCEMENTS
+let pikachuTurn = true;
+function playerTurn () {
+    if (pikachuTurn) {
+        const updatePlayerTurn = document.getElementById("announcement")
+        updatePlayerTurn.innerText = "It's Pikachu turn!"
+    } else if (pikachuTurn == false) {
+        const updatePlayerTurn = document.getElementById("announcement")
+        updatePlayerTurn.innerText = "It's Charmander turn!"
+    }
+}
+
+
 //PIKACHU ATTACKING 
 function pikachuAttack(attacker, defender){
     if (attacker === "pikachu"){
@@ -26,10 +39,13 @@ function pikachuAttack(attacker, defender){
         charmander.health = newHealth
         const healthBar = document.getElementById("charmander-health")
         healthBar.innerText = newHealth
+        pikachuTurn = false;
+        playerTurn()
     } 
     if (charmander.health <= 0){
         const newScore = document.getElementById("pikachu-score")
         newScore.innerText = parseInt(newScore.innerText) + 1
+        resetGame()
         console.log("PIKACHU WIN!")
     }
 }
@@ -44,11 +60,22 @@ function charmanderAttack (attacker, defender) {
         }
         const healthBar = document.getElementById("pikachu-health")
         healthBar.innerText = newHealth
+        pikachuTurn = true;
+        playerTurn()
     }
     if (pikachu.health <= 0){
         const newScore = document.getElementById("charmander-score")
         newScore.innerText = parseInt(newScore.innerText) + 1
+        resetGame()
         console.log("CHARMANDER WIN!")
     }
+}
+
+//RESETTING GAME AFTER SCORING
+function resetGame() {
+    document.getElementById("pikachu-health").innerText = 50;
+    document.getElementById("charmander-health").innerText = 50;
+    pikachu.health = 50;
+    charmander.health = 50;
 }
 
