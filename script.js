4f381eae74f008a28b72691e1c27fe963f7622f6
Ying <ying__li@hotmail.com>
2022-10-27 00:07:33 -0400

    new-additions

--- script.js
+++ script.js
@@ -31,16 +31,19 @@ function playerTurn () {
 function pikachuAttack(attacker, defender){
     if (attacker === "pikachu"){
         console.log("in the pikachu attack")
-        let newHealth = Math.round(charmander.health -= pikachu.damage())
-        if (newHealth <= 0) {
-            newHealth = 0
+        if (pikachuTurn){
+            let newHealth = Math.round(charmander.health -= pikachu.damage())
+            if (newHealth <= 0) {
+                newHealth = 0
+            }
+            charmander.health = newHealth
+            const healthBar = document.getElementById("charmander-health")
+            healthBar.innerText = newHealth
+            pikachuTurn = false;
+            playerTurn()
         }
-        charmander.health = newHealth
-        const healthBar = document.getElementById("charmander-health")
-        healthBar.innerText = newHealth
-        pikachuTurn = false;
-        playerTurn()
     } 
+
     if (charmander.health <= 0){
         const newScore = document.getElementById("pikachu-score")
         newScore.innerText = parseInt(newScore.innerText) + 1
@@ -55,15 +58,18 @@ function pikachuAttack(attacker, defender){
 function charmanderAttack (attacker, defender) {
     if (attacker === "charmander"){
         console.log("in the charmander attack")
-        let newHealth = Math.round(pikachu.health -= charmander.damage())
-        if(newHealth <= 0) {
-            newHealth = 0
+        if (pikachuTurn === false) {
+            let newHealth = Math.round(pikachu.health -= charmander.damage())
+            if(newHealth <= 0) {
+                newHealth = 0
+            }
+            const healthBar = document.getElementById("pikachu-health")
+            healthBar.innerText = newHealth
+            pikachuTurn = true;
+            playerTurn()
         }
-        const healthBar = document.getElementById("pikachu-health")
-        healthBar.innerText = newHealth
-        pikachuTurn = true;
-        playerTurn()
     }
+
     if (pikachu.health <= 0){
         const newScore = document.getElementById("charmander-score")
         newScore.innerText = parseInt(newScore.innerText) + 1
@@ -80,4 +86,10 @@ function resetGame() {
     document.getElementById("charmander-health").innerText = 50;
     pikachu.health = 50;
     charmander.health = 50;
+}
+
+//RESET SCOREBOARD
+function resetButton() {
+    document.getElementById("scoreboard-score") = 0;
+
 }
