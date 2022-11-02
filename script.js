15eb38cd94195553525f522fc737b08f90db296e
Ying <ying__li@hotmail.com>
2022-11-02 19:22:05 -0400

    reset-button

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ function playerTurn () {
 }
 
 //PIKACHU ATTACKING 
-function pikachuAttack(fieldName, attacker, attackName){
+function pikachuAttack(attackCounter, attacker, attackName){
     if (attacker === "pikachu"){
         if (pikachuTurn){
-            const attackCheck = decreaseAbility(fieldName) 
+            const attackCheck = decreaseAbility(attackCounter) 
             if (attackCheck === true){
                 let newHealth = Math.round(charmander.health -= calculateAttack(attackName))
                 if (newHealth <= 0) {
@@ -43,18 +43,17 @@ function pikachuAttack(fieldName, attacker, attackName){
                 newScore.innerText = parseInt(newScore.innerText) + 1
                 const updatePlayerTurn = document.getElementById("announcement")
                 updatePlayerTurn.innerText = "PIKACHU WINS!"
-                resetGame()
-                console.log("PIKACHU WIN!")
+                resetStats()
             }
         }
     }      
 }
 
 //CHARMANDER ATTACKING
-function charmanderAttack (fieldName, attacker, attackName) {
+function charmanderAttack (attackCounter, attacker, attackName) {
     if (attacker === "charmander"){
         if (pikachuTurn === false) {
-            const attackCheck = decreaseAbility(fieldName)
+            const attackCheck = decreaseAbility(attackCounter)
             if (attackCheck === true) {
                 let newHealth = Math.round(pikachu.health -= calculateAttack(attackName))
                 if(newHealth <= 0) {
@@ -72,8 +71,7 @@ function charmanderAttack (fieldName, attacker, attackName) {
                 newScore.innerText = parseInt(newScore.innerText) + 1
                 const updatePlayerTurn = document.getElementById("announcement")
                 updatePlayerTurn.innerText = "CHARMANDER WINS!"
-                resetGame()
-                console.log("CHARMANDER WIN!")
+                resetStats()
             }
         }
 }
@@ -111,7 +109,7 @@ function calculateAttack(nameOfAttack){
 }
 
 //RESETTING GAME AFTER SCORING
-function resetGame() {
+function resetStats() {
     //PIKACHU
     document.getElementById("pikachu-health").innerText = 100;
     document.getElementById("p-counter").innerText = 10;
@@ -129,7 +127,25 @@ function resetGame() {
     charmander.health = 100;
 }
 
-//RESET SCOREBOARD
-// function resetButton() {
-//     document.getElementById("scoreboard-score") = 0;
-// }
+//RESET GAME
+function resetGame() {
+    //RESET PIKACHU
+    document.getElementById("pikachu-health").innerText = 100;
+    document.getElementById("p-counter").innerText = 10;
+    document.getElementById("thunderbolt-counter").innerText = 3;
+    document.getElementById("volt-tackle-counter").innerText = 2;
+    document.getElementById("thunderstorm-counter").innerText = 1;
+    pikachu.health = 100;
+
+    //RESET CHARMANDER
+    document.getElementById("charmander-health").innerText = 100;
+    document.getElementById("c-counter").innerText = 10;
+    document.getElementById("blaze-counter").innerText = 3;
+    document.getElementById("ember-counter").innerText = 2;
+    document.getElementById("fire-blast-counter").innerText = 1;
+    charmander.health = 100;
+
+    //CLEAR SCORE
+    document.getElementById("pikachu-score").innerText = 0;
+    document.getElementById("charmander-score").innerText = 0;
+}
